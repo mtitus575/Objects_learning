07649a73f2da47fcf58e968eecee749682e126a2
Martin T <mtitus575@gmail.com>
2025-03-19 16:56:58 +0000

    Completed the meal maker challenge. Code works as expected.

--- meal_maker.js
+++ meal_maker.js
@@ -48,3 +48,31 @@
      */
 
 //Add code below:
+const menu ={
+    _meal: '',
+    _price: 0,
+    set meal (mealToCheck) {
+        if (typeof mealToCheck === 'string') {
+            return this._meal = mealToCheck;
+        }
+    },
+    set price (priceToCheck) {
+        if (typeof priceToCheck === 'number') {
+            return this._price = priceToCheck;
+        }
+    },
+    get todaysSpecial () {
+        if (this._meal && this._price) {
+            return `Today's special is a ${this._meal} for ${this._price}!`
+        } else {
+            return  'Meal or price was not set correctly!'
+        }
+    }
+};
+
+menu.meal = 'Burger';
+menu.price = '10.99';
+
+
+console.log(menu.todaysSpecial)
+
